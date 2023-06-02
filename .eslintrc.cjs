24ba8db3dd5557488edf156a6f8964c4184218d0
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-02 23:52:06 +0600

    added to db

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,53 @@ import './App.css'
 function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
 
-  return (
-    <>
-      
-      <h1>Users Management</h1>
-      
-      <h3>Numbers Of Users: {users.length}</h3>
-      {
-        users.map(user => user.name)
-      }
-    </>
-  )
-}
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
 
-export default App
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        const newUsers = [...users, data];
+        setUsers(newUsers)
+        form.reset();
+      })
+  }
+
+    return (
+      <>
+
+        <h1>Users Management</h1>
+        <h3>Numbers Of Users: {users.length}</h3>
+        <form onSubmit={handleAddUser}>
+          <input type="text" name='name' placeholder='name' />
+          <br />
+          <input type="email" name="email" placeholder='email' id="" />
+          <br />
+          <input type="submit" value="Add User" />
+        </form>
+        {
+          users.map(user => <p key={user.id}>{user.id}- {user.name} : {user.email}</p>)
+        }
+      </>
+    )
+  }
+
+  export default App
